0f19c1ee71a5560bb88633d800cce885328bd706
nguyen-minh-tri <tringuyen293@gmail.com>
2019-05-18 22:14:36 +0700

    Update clear guidedance to restart the data

--- bikeshare.py
+++ bikeshare.py
@@ -213,7 +213,7 @@ def main():
         user_stats(df, city)
         display_data(df)
 
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        restart = input('\nWould you like to explore the data again? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
 
