f1b884792aba02d4f3607549b9b270bb288be6d0
nguyen-minh-tri <tringuyen293@gmail.com>
2019-05-18 23:47:10 +0700

    improve code day_of_week

--- bikeshare.py
+++ bikeshare.py
@@ -58,8 +58,8 @@ def load_data(city, month, day):
 
     # extract month and day of week from Start Time to create new columns
     df['month'] = df['Start Time'].apply(lambda x: x.month)
-    df['day_of_week'] = df['Start Time']
-                        .apply(lambda x: x.strftime('%A').lower())
+    df['day_of_week'] = (df['Start Time']
+                        .apply(lambda x: x.strftime('%A').lower()))
 
 
     # filter by month if applicable
